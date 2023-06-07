2c5e787cf1f9b12aa1ba4a8d613d95a98e7946c2
Roman <Chepkinrv@mail.ru>
2023-06-07 14:23:57 +0700

    homework_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,4 +1,9 @@
+import json
+from pathlib import Path
+
 from django.shortcuts import render
+
+BASE_DIR = Path(__file__).resolve().parent
 
 def index(request):
     context = {
@@ -9,6 +14,8 @@ def index(request):
 
 def products(request):
     context = {
-        'title': 'GeekShop - Каталог'
+        'title': 'GeekShop - Каталог',
+        'catalog': ['Новинки', 'Одежда', 'Обувь', 'Аксессуары', 'Подарки'],
+        'products': json.load(open(BASE_DIR / 'fixtures/products.json', encoding='utf-8'))
     }
     return render(request, 'mainapp/products.html', context)
